af5bcda2bba20873f3d914d029dec491d39465e1
rommguy <rommguy@gmail.com>
2017-11-12 23:51:13 +0200

    part1 solution

--- part1/part1.js
+++ part1/part1.js
@@ -1,1 +1,35 @@
+// 1
+let myNumber = 0
+
+// 2
+myNumber = myNumber + 1
+myNumber ++
+myNumber += 3
+
+// 3
 const x = 5
+// const x = 6 // error
+
+
+// 4
+const y = 5
+// y = 10 //error
+
+
+// 5
+const first = 10, second = 5
+const product = first * second
+const sum = first + second
+product
+sum
+
+// 6
+const str = 'Hello'
+const str2 = ' World!'
+str + str2 // 'Hello World!'
+
+// 7
+const num = 15
+const prefix = 'Bla bla'
+suffix + num  // Bla bla15
+
