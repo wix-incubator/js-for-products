3e3b5b9b7469389d8036942ece2b102a2d842ad6
rommguy <rommguy@gmail.com>
2017-11-13 20:35:48 +0200

    fix part 2

--- part2/part2-task.js
+++ part2/part2-task.js
@@ -1,22 +1,3 @@
-/*
- * copy paste this to the snippets area in the dev tools
- *
- * if both numbers are even: set the product of x and y to result variable
- * else, set the sum of x and y to result variable
- *
- * Hint - a number is even if it devides by 2 with no remainder
- * x % 2 === 0   <=> x is even
- *
- * examples:
- *
- * multipleOrAdd(2, 4) -> 8
- * multipleOrAdd(6, 4) -> 24
- * multipleOrAdd(6, 3) -> 9
- * multipleOrAdd(3, 6) -> 9
- * multipleOrAdd(1, 10) -> 11
- *
- */
-
 function multipleOrAdd(x, y) {
     let result
 
