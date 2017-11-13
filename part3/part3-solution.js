17c2b198db42c32a2f7fc76c66d95d0cf65ba7ac
rommguy <rommguy@gmail.com>
2017-11-13 10:43:05 +0200

    part3

---
+++ part3/part3-solution.js
@@ -0,0 +1,15 @@
+// TODO implement bartThis. print a text 3 times to the console
+function bartThis(text) {
+    console.log(text)
+    console.log(text)
+    console.log(text)
+}
+
+// TODO implement max - find the greater number our ot 2 numbers
+function max(x, y) {
+    if (x >= y) {
+        return x
+    }
+
+    return y
+}
---
+++ part3/part3-task.js
@@ -0,0 +1,3 @@
+// TODO implement bartThis. print a text 3 times to the console
+
+// TODO implement max - find the greater number our ot 2 numbers
